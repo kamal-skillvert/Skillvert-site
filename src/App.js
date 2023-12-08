91270e4028478be86f417b07710943b930e1831e
kamal-skillvert <neelkamal@skillvert.com>
2023-12-08 15:09:29 +0530

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="./Assets/skillvert_logo.png" className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="Assets/skillvert_logo.png"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Skillver Coming soon
         </a>
       </header>
     </div>
